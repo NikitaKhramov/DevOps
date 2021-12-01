d176655dfa50cfa8f499b5f5a57b1743160e09e8
NikitaKhramov <mantecanikita@gmail.com>
2021-12-01 18:08:33 +0200

    add lab5

---
+++ lab5/hometask5.sh
@@ -0,0 +1,18 @@
+# Create test folders
+mkdir /var/folder{1,2}
+
+# Copy service file
+cat /home/vagrant/file-mover.service > /etc/systemd/system/file-mover.service
+
+# Reload all daemons
+systemctl daemon-reload
+
+# Run the daemon
+systemctl start file-mover.service
+
+# Enable autostart
+systemctl enable file-mover.service
+
+
+# Change directory to cron.d
+cd /etc/cron.d
